e278e97ce6004872e9bfc412549c397757de0281
Alessio Di Pasquale <38081627+alessiodipasquale@users.noreply.github.com>
2022-02-13 19:03:59 +0100

    Initial Commit

---
+++ ToDoSwift/ToDoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftApp.swift
+//  ToDoSwift
+//
+//  Created by Alessio Di Pasquale on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
